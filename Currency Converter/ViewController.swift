302580a329a1c33fa00db5e47df0936e96d58621
Dmytro Ivanchuk <dmytroivanchuk1998@gmail.com>
2022-09-06 12:50:59 +0300

    configure all UI elements in ViewController

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -49,8 +49,8 @@ class ViewController: UIViewController {
         rateView.layer.borderWidth = 1.0
         rateView.layer.borderColor = UIColor.systemGray5.cgColor
         rateView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
-        rateView.layer.shadowOffset = CGSize(width: 0, height: 4)
-        rateView.layer.shadowRadius = 4
+        rateView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        rateView.layer.shadowRadius = 2
         rateView.layer.shadowOpacity = 1
     }
     
@@ -69,9 +69,9 @@ class ViewController: UIViewController {
     private func configureCurrencyButtons() {
         var confiquration = UIButton.Configuration.plain()
         confiquration.image = UIImage(systemName: "chevron.right")
-        confiquration.imagePadding = 5
+        confiquration.imagePadding = 10.0
         confiquration.imagePlacement = .trailing
-        confiquration.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(scale: .medium)
+        confiquration.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(scale: .small)
         confiquration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
             var outgoing = incoming
             outgoing.font = UIFont(name: "Lato-Regular", size: 14)
@@ -91,23 +91,61 @@ class ViewController: UIViewController {
     }
     
     private func configureCurrencyTextFields() {
-        
+        [firstCurrencyTextField, secondCurrencyTextField, thirdCurrencyTextField].forEach {
+            $0?.backgroundColor = UIColor(red: 0.98, green: 0.969, blue: 0.992, alpha: 1)
+            $0?.borderStyle = .none
+            $0?.layer.cornerRadius = 6.0
+        }
     }
     
     private func configureAddCurrencyButton() {
-        
+        var confiquration = UIButton.Configuration.plain()
+        confiquration.title = "Add Currency"
+        confiquration.image = UIImage(systemName: "plus.circle.fill")
+        confiquration.imagePadding = 6.0
+        confiquration.imagePlacement = .leading
+        confiquration.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(scale: .small)
+        confiquration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont(name: "Lato-Regular", size: 13)
+            return outgoing
+        }
+        addCurrencyButton.configuration = confiquration
     }
     
     private func configureShareButton() {
-        
+        var confiquration = UIButton.Configuration.plain()
+        confiquration.image = UIImage(systemName: "square.and.arrow.up")
+        confiquration.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 20.0)
+        confiquration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        confiquration.baseForegroundColor = .systemGray
+        shareButton.configuration = confiquration
     }
     
     private func configureUpdateInfoLabel() {
-        
+        updateInfoLabel.numberOfLines = 0
+        updateInfoLabel.font = UIFont(name: "Lato-Regular", size: 12)
+        updateInfoLabel.textColor = UIColor(red: 0.342, green: 0.342, blue: 0.342, alpha: 1)
+        let titleText = "Last Updated\n22 May 2021 7:03 PM"
+        let attrString = NSMutableAttributedString(string: titleText)
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 7.0
+        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSRange(location: 0, length: titleText.count))
+        updateInfoLabel.attributedText = attrString
     }
     
     private func configureLastYearRateButton() {
-        
+        var confiquration = UIButton.Configuration.plain()
+        confiquration.title = "National Bank Exchange Rate"
+        confiquration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont(name: "Lato-Bold", size: 18)
+            return outgoing
+        }
+        lastYearRateButton.configuration = confiquration
+        lastYearRateButton.layer.borderWidth = 1.0
+        lastYearRateButton.layer.borderColor = UIColor.systemBlue.cgColor
+        lastYearRateButton.layer.cornerRadius = 14.0
     }
 }
 
